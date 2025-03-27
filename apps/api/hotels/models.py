591770981394b8fa994b746e99dc2ea5c3e62e0c
OscarLTC <oscartazacordero@gmail.com>
2025-03-26 19:07:36 -0500

    chore: remove pull_request trigger from workflow and add created_at field to TestModel

--- apps/api/hotels/models.py
+++ apps/api/hotels/models.py
@@ -6,6 +6,7 @@ class TestModel(models.Model):
   description = models.TextField()
   price = models.DecimalField(max_digits=10, decimal_places=2)
   available = models.BooleanField(default=True)
+  created_at = models.DateTimeField(auto_now_add=True)
 
 
   class Meta:
